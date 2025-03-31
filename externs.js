7bedf48d26ea7287f433b96bd8bbf77e57ae014c
Tobias Bannwart <tobias.bannwart@bison-group.com>
2025-03-31 17:10:02 +0200

    Add externs.js for type definitions, update version to 1.1.3, and enhance build script with externs support

---
+++ externs.js
@@ -0,0 +1,39 @@
+/**
+ * @constructor
+ */
+var QrCreator = function() {};
+
+/**
+ * @type {Object}
+ */
+QrCreator.prototype.settings;
+
+/**
+ * @type {string|null}
+ */
+QrCreator.prototype.settings.cornerColor;
+
+/**
+ * @type {Object}
+ */
+QrCreator.prototype.settings.icon;
+
+/**
+ * @type {string}
+ */
+QrCreator.prototype.settings.icon.src;
+
+/**
+ * @type {number}
+ */
+QrCreator.prototype.settings.icon.width;
+
+/**
+ * @type {number}
+ */
+QrCreator.prototype.settings.icon.height;
+
+/**
+ * @type {string}
+ */
+QrCreator.prototype.settings.icon.crossOrigin;
